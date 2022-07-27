9a2f51bb14b750b5e9c3a36067d771cf8cfd0ba4
adsds126 <110022522+adsds126@users.noreply.github.com>
2022-07-27 18:10:11 +0900

    Add files via upload

---
+++ src/Chapter1.java
@@ -0,0 +1,13 @@
+public class Chapter1 {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 10;
+        int tmp;
+        tmp = x;
+        x = y;
+        y = tmp;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
