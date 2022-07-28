8520cab869d87705bca49da56452c58f3490b1ce
김태형 <gojobtae@gimtaehyeongui-MacBook.local>
2022-07-28 23:56:02 +0900

    Chapter1-6

---
+++ src/Chapter6TypeDemo.java
@@ -0,0 +1,12 @@
+public class Chapter6TypeDemo {
+    public static void main(String[] args) {
+        System.out.println(3 +'0'); //'0'은 숫자로48
+        System.out.println((char)(3 + '0'));//형변환
+        System.out.println("3".charAt(0) - '0');
+        System.out.println(Integer.parseInt("3")+1);
+        System.out.println("3"+1);
+        System.out.println('3'-'0'+1);
+
+
+    }
+}
