6aef09c25fefe29fa2ab446f3992fb2876c53fcb
김태형 <gojobtae@gimtaehyeongui-MacBook.local>
2022-07-29 12:31:42 +0900

    fix: config

---
+++ src/Chapter7OperatorDemo.java
@@ -0,0 +1,8 @@
+public class Chapter7OperatorDemo {
+    public static void main(String[] args) {
+       int x ,y;
+       x = y = 3;
+        System.out.println(y);
+        System.out.println(x);
+    }
+}
